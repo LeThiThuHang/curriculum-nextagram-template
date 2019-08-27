9d676676fc367278ccb09fb9ed707c1872f9b956
LeThiThuHang <thuhang.lit@gmail.com>
2019-08-27 12:27:49 +0800

    finnish day1 flask

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,19 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, request,redirect, url_for
 
+from models.user import User
 
 users_blueprint = Blueprint('users',
                             __name__,
                             template_folder='templates')
 
-
+#this is to show the sign up form
 @users_blueprint.route('/new', methods=['GET'])
 def new():
     return render_template('users/new.html')
 
-
+# this is to take in new user inforamtion to the database
 @users_blueprint.route('/', methods=['POST'])
 def create():
-    pass
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+
+    new_user = User(name=name, email=email, password = password)
+
+    if new_user.save():
+        flash('User has been created successfully','success')
+        return redirect(url_for('users.new'))
+    else: 
+        flash('Please try again!','danger')
+        return render_template('users/new.html' , errors = new_user.errors)
+
 
 
 @users_blueprint.route('/<username>', methods=["GET"])
--- models/user.py
+++ models/user.py
@@ -1,6 +1,40 @@
 from models.base_model import BaseModel
 import peewee as pw
+from werkzeug.security import generate_password_hash
+
+# for testing password 
+import re 
 
 
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    name = pw.CharField(unique=True) #unique, not allow null
+    email = pw.CharField(unique=True, index=True) #index column, unique, not allow null
+    password = pw.CharField() # no need to be unique
+
+    def validate(self):
+        duplicate_names = User.get_or_none(User.name == self.name)
+        duplicate_emails = User.get_or_none(User.email == self.email)
+
+
+        if duplicate_names:
+            self.errors.append('Name is not unique')
+            
+        if duplicate_emails:
+            self.errors.append('Email is not unique')
+    
+        if len(self.password) < 6:
+            self.errors.append('Password must be at least 6 characters')
+
+        if re.search(r"[A-Z]", self.password) is None: 
+            self.errors.append('Password must contains at least 1 upper case')
+        
+        if re.search(r"\d", self.password) is None: 
+            self.errors.append('Password must contains at least 1 digit')
+        
+        if re.search(r"[!@$]", self.password) is None: 
+            self.errors.append('Password must contains at least 1 symbol')
+        else:
+            self.password = generate_password_hash(self.password)
+
+
+
